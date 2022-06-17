520fafe32243681c4fab55eebdef165f57312c47
PhoenixGS <thestarrydream@gmail.com>
2022-06-17 17:03:53 +0800

    修改信息

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'PHOENIXGS',
+  author: 'PhoenixGS',
+  email: 'thestarrydream@gmail.com',
+  link: 'https://www.phoenixgs.cn',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
